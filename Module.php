e48bb7b967b45974cabf27ee02b476f52905e973
Александр Иванов <a.ivanov@iml.ru>
2016-05-29 20:43:38 +0300

    Zendify component

---
+++ Module.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Petrinet;
+
+use Zend\Loader;
+use Zend\Console\Adapter\AdapterInterface;
+use Zend\ModuleManager\Feature;
+
+/**
+ * Petrinet
+ */
+class Module implements
+    Feature\ConfigProviderInterface,
+    Feature\ConsoleBannerProviderInterface,
+    Feature\ConsoleUsageProviderInterface,
+    Feature\DependencyIndicatorInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getModuleDependencies()
+    {
+        return array('DoctrineORMModule');
+    }
+}
